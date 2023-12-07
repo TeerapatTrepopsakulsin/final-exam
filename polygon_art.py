9ecfe70aa31a2168a3375b736dc5db958ca069bb
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2023-12-07 11:48:11 +0700

    get rid of unnessesary code fragment

--- polygon_art.py
+++ polygon_art.py
@@ -37,52 +37,6 @@ class Draw:
     def get_new_position(self):
         self.location = [random.randint(-300, 300), random.randint(-200, 200)]
 
-# def draw_polygon(num_sides, size, orientation, location, color, border_size):
-#     turtle.penup()
-#     turtle.goto(location[0], location[1])
-#     turtle.setheading(orientation)
-#     turtle.color(color)
-#     turtle.pensize(border_size)
-#     turtle.pendown()
-#     for _ in range(num_sides):
-#         turtle.forward(size)
-#         turtle.left(360/num_sides)
-#     turtle.penup()
-
-# def get_new_color():
-#     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-
-
-# draw a polygon at a random location, orientation, color, and border line thickness
-# num_sides = random.randint(3, 5) # triangle, square, or pentagon
-# size = random.randint(50, 150)
-# orientation = random.randint(0, 90)
-# location = [random.randint(-300, 300), random.randint(-200, 200)]
-# color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-# border_size = random.randint(1, 10)
-# draw_polygon(num_sides, size, orientation, location, color, border_size)
-#
-# # specify a reduction ratio to draw a smaller polygon inside the one above
-# reduction_ratio = 0.618
-#
-# # reposition the turtle and get a new location
-# turtle.penup()
-# turtle.forward(size*(1-reduction_ratio)/2)
-# turtle.left(90)
-# turtle.forward(size*(1-reduction_ratio)/2)
-# turtle.right(90)
-# location[0] = turtle.pos()[0]
-# location[1] = turtle.pos()[1]
-#
-# # adjust the size according to the reduction ratio
-# size *= reduction_ratio
-#
-# # draw the second polygon embedded inside the original
-# draw_polygon(num_sides, size, orientation, location, color, border_size)
-#
-# # hold the window; close it by clicking the window close 'x' mark
-# turtle.done()
 
 choice = input('Which art do you want to generate? Enter a number between 1 to 8, inclusive: ')
 if choice == '1':
@@ -183,5 +137,3 @@ if choice == '8':
             a.size *= a.reduction_ratio
             a.polygon()
     turtle.done()
-
-
