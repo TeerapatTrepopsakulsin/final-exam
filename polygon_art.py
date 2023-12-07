4e95509859c3622fc01b867d9f24a9529b17cf06
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2023-12-07 11:29:28 +0700

    other choice completed

--- polygon_art.py
+++ polygon_art.py
@@ -37,17 +37,17 @@ class Draw:
     def get_new_position(self):
         self.location = [random.randint(-300, 300), random.randint(-200, 200)]
 
-def draw_polygon(num_sides, size, orientation, location, color, border_size):
-    turtle.penup()
-    turtle.goto(location[0], location[1])
-    turtle.setheading(orientation)
-    turtle.color(color)
-    turtle.pensize(border_size)
-    turtle.pendown()
-    for _ in range(num_sides):
-        turtle.forward(size)
-        turtle.left(360/num_sides)
-    turtle.penup()
+# def draw_polygon(num_sides, size, orientation, location, color, border_size):
+#     turtle.penup()
+#     turtle.goto(location[0], location[1])
+#     turtle.setheading(orientation)
+#     turtle.color(color)
+#     turtle.pensize(border_size)
+#     turtle.pendown()
+#     for _ in range(num_sides):
+#         turtle.forward(size)
+#         turtle.left(360/num_sides)
+#     turtle.penup()
 
 # def get_new_color():
 #     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
@@ -96,18 +96,92 @@ if choice == '1':
         a.polygon()
     turtle.done()
 if choice == '2':
-    pass
+    turtle.speed(0)
+    turtle.bgcolor('black')
+    turtle.tracer(0)
+    turtle.colormode(255)
+    for _ in range(30):
+        a = Draw(4, 0.618, [0, 0])
+        a.get_new_position()
+        a.polygon()
+    turtle.done()
 if choice == '3':
-    pass
+    turtle.speed(0)
+    turtle.bgcolor('black')
+    turtle.tracer(0)
+    turtle.colormode(255)
+    for _ in range(30):
+        a = Draw(5, 0.618, [0, 0])
+        a.get_new_position()
+        a.polygon()
+    turtle.done()
 if choice == '4':
-    pass
+    turtle.speed(0)
+    turtle.bgcolor('black')
+    turtle.tracer(0)
+    turtle.colormode(255)
+    for _ in range(30):
+        num_side = random.randint(3, 5)
+        a = Draw(num_side, 0.618, [0, 0])
+        a.get_new_position()
+        a.polygon()
+    turtle.done()
 if choice == '5':
-    pass
+    turtle.speed(0)
+    turtle.bgcolor('black')
+    turtle.tracer(0)
+    turtle.colormode(255)
+    for _ in range(30):
+        a = Draw(3, 0.618, [0, 0])
+        a.get_new_position()
+        a.polygon()
+        a.reposition()
+        for i in range(2):
+            a.size *= a.reduction_ratio
+            a.polygon()
+    turtle.done()
 if choice == '6':
-    pass
+    turtle.speed(0)
+    turtle.bgcolor('black')
+    turtle.tracer(0)
+    turtle.colormode(255)
+    for _ in range(30):
+        a = Draw(4, 0.618, [0, 0])
+        a.get_new_position()
+        a.polygon()
+        a.reposition()
+        for i in range(2):
+            a.size *= a.reduction_ratio
+            a.polygon()
+    turtle.done()
 if choice == '7':
-    pass
+    turtle.speed(0)
+    turtle.bgcolor('black')
+    turtle.tracer(0)
+    turtle.colormode(255)
+    for _ in range(30):
+        a = Draw(5, 0.618, [0, 0])
+        a.get_new_position()
+        a.polygon()
+        a.reposition()
+        for i in range(2):
+            a.size *= a.reduction_ratio
+            a.polygon()
+    turtle.done()
 if choice == '8':
-    pass
+    turtle.speed(0)
+    turtle.bgcolor('black')
+    turtle.tracer(0)
+    turtle.colormode(255)
+    for _ in range(30):
+        num_side = random.randint(3, 5)
+        a = Draw(num_side, 0.618, [0, 0])
+        a.get_new_position()
+        a.polygon()
+        a.reposition()
+        for i in range(2):
+            a.size *= a.reduction_ratio
+            a.polygon()
+    turtle.done()
 
 
