57f0de4ae5370f74e608f4d5398abbe7c1348bcb
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2023-12-07 10:23:03 +0700

    initiate

--- polygon_art.py
+++ polygon_art.py
@@ -1,6 +1,27 @@
 import turtle
 import random
 
+class Draw:
+    def __init__(self, num_sides, size, orientation, location, color, border_size):
+        self.num_sides = num_sides
+        self.size = size
+        self.orientation = orientation
+        self.location = location
+        self.color = color
+        self.border_size = border_size
+
+    def polygon(self):
+        turtle.penup()
+        turtle.goto(self.location[0], self.location[1])
+        turtle.setheading(self.orientation)
+        turtle.color(self.color)
+        turtle.pensize(self.border_size)
+        turtle.pendown()
+        for _ in range(self.num_sides):
+            turtle.forward(self.size)
+            turtle.left(360 / self.num_sides)
+        turtle.penup()
+
 def draw_polygon(num_sides, size, orientation, location, color, border_size):
     turtle.penup()
     turtle.goto(location[0], location[1])
